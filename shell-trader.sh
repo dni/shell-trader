12cef89e3e30437ff438f1c22a286a430c10e162
dni <dni.khr@gmail.com>
2021-12-21 18:56:28 +0100

    cleanup

--- shell-trader.sh
+++ shell-trader.sh
@@ -1,38 +1,10 @@
 #/usr/bin/env sh
-data=$(tail -n 1 signal-log.txt)
-price=$(cut -d "," -f 1)
-order_type=$(cut -d "," -f 2)
-side=$(cut -d "," -f 3)
-
-close_all() {
-  send_request "DELETE" "/v1/futures/all/close"
-}
-cancel_all() {
-  send_request "DELETE" "/v1/futures/all/cancel"
-}
-
-send_test() {
-  send_request "POST" "/v1/futures" '{"type":"l","side":"b","price":40000,"quantity":1,"leverage":10}'
-}
-
-
-# sample create request on futures
-#{
-#
-#    "type": "l", market or limit order
-#    "side": "b", buy and sell
-#    "price": 10000,
-#    "stoploss": 9000,
-#    "takeprofit": 11000,
-#    "quantity": 100,
-#    "leverage": 50
-#
-#}
+json_string=$(tail -n 1 signal-log.txt)
+source ./.env
 
 send_request() {
   method=$1
   path=$2
-  json_string=$3
   ts=$(date +"%s")000
   signed_message=$(echo -n "$ts$method$path$json_string" | openssl dgst -sha256 -hmac $API_SECRET -binary | base64 )
   curl $API_ENDPOINT$path \
@@ -45,12 +17,16 @@ send_request() {
     --data "$json_string"
 }
 
-source ./.env
-send_test
+close_all() {
+  send_request "DELETE" "/v1/futures/all/close"
+}
+cancel_all() {
+  send_request "DELETE" "/v1/futures/all/cancel"
+}
+create_order() {
+  send_request "POST" "/v1/futures"
+}
+
+create_order
+# cancel_all
 # close_all
-cancel_all
-#send_request "GET" "/v1/futures/ticker"
-
-
-
-
